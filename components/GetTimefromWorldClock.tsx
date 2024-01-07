8fa30e0cb1bf67fde25552476f023afcbae448e1
suka1557 <suka1557@gmail.com>
2024-01-07 11:25:31 +0530

    Modified GetTime function to calculate time gap in seconds between local time and time at selected location and return it

--- components/GetTimefromWorldClock.tsx
+++ components/GetTimefromWorldClock.tsx
@@ -5,18 +5,26 @@ let WORLDCLOCK_API = 'http://worldtimeapi.org/api/timezone/' ;
 export const GetTimeatLocation = async (timezone: string) => {
   
   const timezone_uri = WORLDCLOCK_API + timezone ;
-  let location_date_time = null ;
+  let location_date_time = moment() ;
+  let calculated_time_gap_in_seconds = 0 ;
+  // console.log('Time in local system: ', location_date_time.format('HH:mm:ss')) ;
 
     try {
       // Replace 'YOUR_API_ENDPOINT' with the actual API endpoint URL
       const response = await fetch(timezone_uri);
       const data = await response.json();
-      let location_date_time = moment.parseZone(data.datetime) ;
+      location_date_time = moment(data.datetime.substr(0, 19)) ;
+      calculated_time_gap_in_seconds = location_date_time.diff(moment(), 'seconds');
 
-      console.log('Current Time for Selected Location:', timezone, ' : ', location_date_time.format('HH:mm:ss'));
+      // console.log('Response from API: ', data.datetime.substr(0, 19));
+      // console.log('Current Time for Selected Location:', timezone, ' : ', location_date_time.format('HH:mm:ss'));
+      // console.log('Time Difference between India and Selected Location in Seconds : ', location_date_time.diff(moment(), 'seconds'));
+      // console.log('Result of adding 100000 seconds: ', location_date_time.add(100000,'seconds').format('DD-MMM-YYYY HH:mm:ss') );
     } 
     catch (error) {
       console.error('Error fetching API:', error);
-    }
+    };
 
-  }
+    return calculated_time_gap_in_seconds;
+
+  };
